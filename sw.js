933560533bb4fb507ab31c2c2c9fe9befba56485
tickedoffcodess <tickedoffcodess@gmail.com>
2026-03-05 15:06:00 -0800

    Updates

---
+++ sw.js
@@ -0,0 +1,39 @@
+var CACHE_NAME = "pem-toolkit-v1";
+
+self.addEventListener("install", function(e) {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE_NAME; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener("fetch", function(e) {
+  if (e.request.url.startsWith(self.location.origin)) {
+    e.respondWith(
+      caches.match(e.request).then(function(cached) {
+        var fetched = fetch(e.request).then(function(response) {
+          if (response.ok) {
+            var clone = response.clone();
+            caches.open(CACHE_NAME).then(function(cache) {
+              cache.put(e.request, clone);
+            });
+          }
+          return response;
+        }).catch(function() {
+          return cached;
+        });
+        return cached || fetched;
+      })
+    );
+  }
+});
